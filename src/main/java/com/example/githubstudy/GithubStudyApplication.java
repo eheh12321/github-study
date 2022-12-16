1567153941e07f9a2dba59151515150303ab171c
eheh12321 <eheh12321@gmail.com>
2022-12-16 16:55:59 +0900

    Create Project

---
+++ src/main/java/com/example/githubstudy/GithubStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubstudy/GithubStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
